9efd4259bdd34f4132d04750c95a5b6c57cc7153
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-03 18:20:20 +0500

    filters start working

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+let filters = document.querySelector('.filter-pills');
+let eachfilter = filters.querySelectorAll('.pill');
+let type = document.querySelectorAll('.card-badge');
+// debugger
+eachfilter.forEach(pill => {
+    pill.addEventListener('click', () => {
+
+        eachfilter.forEach(btn => btn.classList.remove('active'));
+        
+        pill.classList.add('active');
+        type.forEach(card => {
+            if (card.textContent.trim() === pill.textContent) {
+                card.parentNode.parentNode.style.display = "block";
+            } 
+            else if (pill.textContent === 'All Events'){
+                card.parentNode.parentNode.style.display = "block";
+            }
+            else {
+                card.parentNode.parentNode.style.display = "none";
+            }
+        });
+    });
+});
+
+ document.addEventListener('DOMContentLoaded', function() {
+        flatpickr("#datePicker", {
+            altInput: true,
+            altFormat: "F j, Y",
+            dateFormat: "Y-m-d",
+            minDate: "today",
+            // This ensures the calendar isn't hidden behind other sections
+            // static: true 
+        });
+    });
