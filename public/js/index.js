97e9ee2545ef26b427b8feac0b81d7e2808be7ef
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-10 15:31:44 +0500

    Date filter Perfectly working.

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,45 @@
-let filters = document.querySelector('.filter-pills');
-let eachfilter = filters.querySelectorAll('.pill');
+let eachfilter = document.querySelectorAll('.pill');
 let type = document.querySelectorAll('.card-badge');
 let dateinput = document.getElementById('datePicker');
 
-// debugger
+
+const runFilters = () => {
+    let selectedPill = document.querySelector('.pill.active').getAttribute('btn-type')
+    let selectedDate = dateinput.value; 
+
+    type.forEach(card => {
+        
+        
+        let cardCategory = card.getAttribute('event-type');
+        let cardParent = card.parentNode.parentNode; 
+        let postDate = cardParent.getAttribute('data-date'); 
+        
+        
+        let matchesCategory = (selectedPill === 'All Events' || cardCategory === selectedPill);
+        
+        
+        let matchesDate = (selectedDate === "" || postDate === selectedDate);
+
+        
+        if (matchesCategory && matchesDate) {
+            cardParent.style.display = "block";
+        } 
+        else {
+            cardParent.style.display = "none";
+        }
+
+    });
+};
 
 eachfilter.forEach(pill => {
     pill.addEventListener('click', () => {
-
         eachfilter.forEach(btn => btn.classList.remove('active'));
-
         pill.classList.add('active');
-        type.forEach(card => {
-            if (card.textContent.trim() === pill.textContent) {
-                card.parentNode.parentNode.style.display = "block";
-            }
-            else if (pill.textContent === 'All Events') {
-                card.parentNode.parentNode.style.display = "block";
-            }
-            else {
-                card.parentNode.parentNode.style.display = "none";
-            }
-        });
+        runFilters(); 
     });
 });
 
 
+dateinput.addEventListener('input', () => {
+    runFilters(); 
+});
