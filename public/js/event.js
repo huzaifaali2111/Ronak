47ce4a64c729daffef47e75e70dbca536ec46dff
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-14 17:40:25 +0500

    Enhance event page with joining form and JavaScript functionality; improve CSS styles for better layout

---
+++ public/js/event.js
@@ -0,0 +1,7 @@
+let joining_form = document.getElementById("joining_form");
+let join_cta = document.getElementById("join_cta");
+
+join_cta.addEventListener('click', ()=>{
+    joining_form.style.display = 'block';
+})
+
